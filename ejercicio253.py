938af279aa868c3c98c1e93c4a2ebf8bd65ed797
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-09 11:42:29 -0400

    mi primer comit

---
+++ ejercicio253.py
@@ -0,0 +1,18 @@
+ingreso = True
+while ingreso:
+    print("--Menu--")
+    print("--Menu--")
+    print("--Menu--")
+    print("--Menu--")
+    op = int(input("Ingrese su opción"))
+
+    if op == 1:
+        print("Pganado..")
+    elif op == 2:
+        print("Comprando...")
+    elif op == 3:
+        print("Saliendo")
+        #break
+        ingreso = False
+    else:
+        (print("Opción no valida"))
